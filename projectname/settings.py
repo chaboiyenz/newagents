c5f171fc5849bd3f35e96d0f4d468c4aff1e54d6
chaboiyenz <158559942+chaboiyenz@users.noreply.github.com>
2025-05-29 15:28:29 +0800

    Update settings.py

--- projectname/settings.py
+++ projectname/settings.py
@@ -68,9 +68,12 @@ WSGI_APPLICATION = 'projectname.wsgi.application'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
 DATABASES = {
-    'default': dj_database_url.config(default='postgres://localhost', conn_max_age=600)
+    'default': dj_database_url.config(
+        default=os.getenv('DATABASE_URL'),
+        conn_max_age=600,
+        ssl_require=True
+    )
 }
-
 
 
 # Password validation
